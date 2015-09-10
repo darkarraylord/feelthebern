668eb3c43c7844549ac586ebc1dea95bb7f0e8f2
Gill <gilcpd2@gmail.com>
2015-09-09 22:28:55 -0300

    masonry
    masonry pinterest

---
+++ sites/all/themes/nexus/js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+	// --- masonry --- //
+	var $container = $('.view-content');
+	// initialize
+	$container.masonry({
+		itemSelector: '.masonry-brick'
+	});
+});
